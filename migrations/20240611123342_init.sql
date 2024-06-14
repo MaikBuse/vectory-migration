61bd6f7f4a642df6673809abb554c5a943b4b31d
Maik Buse <contact@maikbuse.com>
2024-06-14 17:52:34 +0200

    feat(user): revise data fields

--- migrations/20240611123342_init.sql
+++ migrations/20240611123342_init.sql
@@ -6,10 +6,13 @@ CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    keycloak_id UUID UNIQUE NOT NULL,
+    oauth_id UUID UNIQUE NOT NULL,
+    forename VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    created_by UUID NOT NULL,
+    updated_by UUID NOT NULL
 );
 -- +goose StatementEnd
 
